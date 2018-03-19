1b2253469f80f59dcf6879a1daaab35b9e933f53
Alex Silyuk <alexesi@ac.sce.ac.il>
2018-03-19 19:19:01 +0200

    init3

--- hw1/Init.py
+++ hw1/Init.py
@@ -20,15 +20,26 @@ class MyTopo( Topo ):
         Topo.__init__( self )
 
         # Add hosts and switches
-        leftHost = self.addHost( 'h1' )
-        rightHost = self.addHost( 'h2' )
-        leftSwitch = self.addSwitch( 's3' )
-        rightSwitch = self.addSwitch( 's4' )
+        h1 = self.addHost( 'h1' )
+        h1
+        h2 = self.addHost( 'h2' )
+        h3 = self.addHost( 'h3' )
+        h4 = self.addHost( 'h4' )
+        s1 = self.addSwitch( 's1' )
+        s2 = self.addSwitch( 's2' )
+        s3 = self.addSwitch( 's3' )
+        s4 = self.addSwitch( 's4' )
 
         # Add links
-        self.addLink( leftHost, leftSwitch )
-        self.addLink( leftSwitch, rightSwitch )
-        self.addLink( rightSwitch, rightHost )
+        self.addLink(h1,s1)
+        self.addLink(h2, s1)
+        self.addLink(s1, s2)
+        self.addLink(s1,s3)
+        self.addLink(s3,s4)
+        self.addLink(s2,s4)
+        self.addLink(s4,h3)
+        self.addLink(s4, h4)
+
 
 
 topos = { 'mytopo': ( lambda: MyTopo() ) }
