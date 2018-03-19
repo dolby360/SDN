d2777cf1a41a5d9850b61b3fba285ea90cfd2d98
Alex Silyuk <alexesi@ac.sce.ac.il>
2018-03-19 20:30:29 +0200

    POX first initialization

--- hw1/Init.py
+++ hw1/Init.py
@@ -10,14 +10,14 @@ class MyTopo( Topo ):
         Topo.__init__( self )
 
         # Add hosts and switches
-        h1 = self.addHost( 'h1' )
-        h2 = self.addHost( 'h2' )
-        h3 = self.addHost( 'h3' )
-        h4 = self.addHost( 'h4' )
-        s1 = self.addSwitch( 's1' )
-        s2 = self.addSwitch( 's2' )
-        s3 = self.addSwitch( 's3' )
-        s4 = self.addSwitch( 's4' )
+        h1 = self.addHost( 'h1' ,mac='00:00:00:00:00:01')
+        h2 = self.addHost( 'h2' ,mac='00:00:00:00:00:02')
+        h3 = self.addHost( 'h3' ,mac='00:00:00:00:00:03')
+        h4 = self.addHost( 'h4' ,mac='00:00:00:00:00:04')
+        s1 = self.addSwitch( 's1')
+        s2 = self.addSwitch( 's2')
+        s3 = self.addSwitch( 's3')
+        s4 = self.addSwitch( 's4')
 
         # Add links
         self.addLink(h1,s1)
--- hw1/p/pox/forwarding/my.py
+++ hw1/p/pox/forwarding/my.py
@@ -1,9 +1,50 @@
 from pox.core import core
+import pox.openflow.libopenflow_01 as of
 from pox.lib.util import dpidToStr
+from pox.openflow import *
 
+table = {}
+all_ports = of.OFPP_FLOOD
+log = core.getLogger()
 
 def _handle_PacketIn(event):
-  print "Received packet from port %d" % event.port
+  packet = event.parsed
+
+  # Learn the source
+  table[(event.connection, packet.src)] = event.port
+
+  dst_port = table.get((event.connection, packet.dst))
+
+  if dst_port is None:
+    # We don't know where the destination is yet.  So, we'll just
+    # send the packet out all ports (except the one it came in on!)
+    # and hope the destination is out there somewhere. :)
+    msg = of.ofp_packet_out(data=event.ofp)
+    msg.actions.append(of.ofp_action_output(port=all_ports))
+    event.connection.send(msg)
+  else:
+    # Since we know the switch ports for both the source and dest
+    # MACs, we can install rules for both directions.
+    msg = of.ofp_flow_mod()
+    msg.match.dl_dst = packet.src
+
+    msg.match.dl_src = packet.dst
+    print "Packet from MAC : " +str(packet.dst)
+
+
+    msg.actions.append(of.ofp_action_output(port=event.port))
+    event.connection.send(msg)
+
+    # This is the packet that just came in -- we want to
+    # install the rule and also resend the packet.
+    msg = of.ofp_flow_mod()
+    msg.data = event.ofp  # Forward the incoming packet
+    msg.match.dl_src = packet.src
+    msg.match.dl_dst = packet.dst
+    msg.actions.append(of.ofp_action_output(port=dst_port))
+    event.connection.send(msg)
+
+    log.debug("Installing %s <-> %s" % (packet.src, packet.dst))
 
 def _handle_ConnectionUp (event):
   print "Switch with dpid=%s connected" % dpidToStr(event.dpid)
