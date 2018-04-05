5370680de02174fc72775604e40a62ea3c3109b2
dolev <dolby360>
2018-04-05 17:52:12 +0300

    TCP

--- sample_controller.py
+++ sample_controller.py
@@ -76,40 +76,42 @@ def arpRequest(event):
 #--------------------------------Route-----------------------------------#
 allMacs = ['00:00:00:00:00:01','00:00:00:00:00:02','00:00:00:00:00:03','00:00:00:00:00:04']
 
-def siteA(event):
+def siteA(event,vid):
   hosts = ['00:00:00:00:00:01','00:00:00:00:00:02']
   hosDict = {'00:00:00:00:00:01':3,'00:00:00:00:00:02':4}
   direction = [
     ['00:00:00:00:00:01','00:00:00:00:00:03',1,False],
-    #['00:00:00:00:00:01','00:00:00:00:00:03',2,True],
+    ['00:00:00:00:00:01','00:00:00:00:00:03',2,True],
     ['00:00:00:00:00:01','00:00:00:00:00:04',1,False],
-    #['00:00:00:00:00:01','00:00:00:00:00:04',2,True],
+    ['00:00:00:00:00:01','00:00:00:00:00:04',2,True],
 
     ['00:00:00:00:00:02','00:00:00:00:00:03',1,False],
-    #['00:00:00:00:00:02','00:00:00:00:00:03',2,True],
+    ['00:00:00:00:00:02','00:00:00:00:00:03',2,True],
     ['00:00:00:00:00:02','00:00:00:00:00:04',1,False],
-    #['00:00:00:00:00:02','00:00:00:00:00:04',2,True],
+    ['00:00:00:00:00:02','00:00:00:00:00:04',2,True],
   ]
   for item in direction:
-    #create flow match rule
-    match = of.ofp_match()
-    match.dl_src = EthAddr(item[0])
-    match.dl_dst = EthAddr(item[1])
+    if vid == item[3]:
+      #create flow match rule
+      match = of.ofp_match()
+      match.dl_src = EthAddr(item[0])
+      match.dl_dst = EthAddr(item[1])
 
-    fm = of.ofp_flow_mod()
-    fm.match = match
-    fm.hard_timeout = 300
-    fm.idle_timeout = 100
-    fm.actions.append(of.ofp_action_output(port=int(item[2])))
-    event.connection.send(fm)
+      fm = of.ofp_flow_mod()
+      fm.match = match
+      fm.hard_timeout = 300
+      fm.idle_timeout = 100
+
+      fm.actions.append(of.ofp_action_output(port=int(item[2])))
+      event.connection.send(fm)
   for macs in allMacs:
     for hos in hosts:
-      if item != hos:
+      if macs != hos:
         #create flow match rule
         match = of.ofp_match()
         match.dl_src = EthAddr(macs)
         match.dl_dst = EthAddr(hos)
-
+        
         fm = of.ofp_flow_mod()
         fm.match = match
         fm.hard_timeout = 300
@@ -117,7 +119,7 @@ def siteA(event):
         fm.actions.append(of.ofp_action_output(port=hosDict[hos]))
         event.connection.send(fm)
 
-def siteB(event):
+def siteB(event,vid):
   #All hosts in this site
   hosts = ['00:00:00:00:00:03','00:00:00:00:00:04']
   #If we want to reach a host, what is the port we need to go to.
@@ -125,30 +127,31 @@ def siteB(event):
   #If we want to get out, so to which "switch" do we need to go?
   direction = [
     ['00:00:00:00:00:03','00:00:00:00:00:01',1,False],
-    #['00:00:00:00:00:03','00:00:00:00:00:01',2,True],
+    ['00:00:00:00:00:03','00:00:00:00:00:01',2,True],
     ['00:00:00:00:00:03','00:00:00:00:00:02',1,False],
-    #['00:00:00:00:00:03','00:00:00:00:00:02',2,True],
+    ['00:00:00:00:00:03','00:00:00:00:00:02',2,True],
 
     ['00:00:00:00:00:04','00:00:00:00:00:01',1,False],
-    #['00:00:00:00:00:04','00:00:00:00:00:01',2,True],
+    ['00:00:00:00:00:04','00:00:00:00:00:01',2,True],
     ['00:00:00:00:00:04','00:00:00:00:00:02',1,False],
-    #['00:00:00:00:00:04','00:00:00:00:00:02',2,True],
+    ['00:00:00:00:00:04','00:00:00:00:00:02',2,True],
   ]
   for item in direction:
-    #create flow match rule
-    match = of.ofp_match()
-    match.dl_src = EthAddr(item[0])
-    match.dl_dst = EthAddr(item[1])
+    if vid == item[3]:
+      #create flow match rule
+      match = of.ofp_match()
+      match.dl_src = EthAddr(item[0])
+      match.dl_dst = EthAddr(item[1])
 
-    fm = of.ofp_flow_mod()
-    fm.match = match
-    fm.hard_timeout = 300
-    fm.idle_timeout = 100
-    fm.actions.append(of.ofp_action_output(port=int(item[2])))
-    event.connection.send(fm)
+      fm = of.ofp_flow_mod()
+      fm.match = match
+      fm.hard_timeout = 300
+      fm.idle_timeout = 100
+      fm.actions.append(of.ofp_action_output(port=int(item[2])))
+      event.connection.send(fm)
   for macs in allMacs:
     for hos in hosts:
-      if item != hos:
+      if macs != hos:
         #create flow match rule
         match = of.ofp_match()
         match.dl_src = EthAddr(macs)
@@ -192,11 +195,11 @@ def noSite(event):
       fm.actions.append(of.ofp_action_output(port=1))
       event.connection.send(fm)
 
-def routeThisPacket(event):
+def routeThisPacket(event,vid):
   if dpidToStr(event.dpid) == '00-00-00-00-00-11':
-    siteA(event)
+    siteA(event,vid)
   elif dpidToStr(event.dpid) == '00-00-00-00-00-14':
-    siteB(event)
+    siteB(event,vid)
   else:
     noSite(event)
 
@@ -227,7 +230,7 @@ def checkIfNeedToDrop(event):
     fm.idle_timeout = 100
     event.connection.send(fm)
 
-    print 'Communication between {} and {} is forbidden'.format(pol[1],pol[2])
+    # print 'Communication between {} and {} is forbidden'.format(pol[1],pol[2])
 
 def getPolicies():
   filePath = os.getcwd() + '/ext/firewall-policies.csv'
@@ -238,14 +241,24 @@ def getPolicies():
         policies.append(row)
 #------------------------------------------------------------------------#
 
-def _handle_PacketIn(event):
-  arpRequest(event)
 
+def routVid(event):
+  msg.actions.append( of.ofp_action_output( port = event.port ) )
+  event.connection.send( msg )
+
+def _handle_PacketIn(event):
+  #print "packet in to - %s" % dpidToStr(event.dpid)
+
+  if dpidToStr(event.dpid) == '00-00-00-00-00-11':
+    tcpp = event.parsed.find('tcp') 
+    if tcpp is not None: 
+      if tcpp.dstport == 10000:
+        routVid(event)
+    
 def _handle_ConnectionUp (event):
   print "Switch with dpid=%s connected" % dpidToStr(event.dpid)
-  routeThisPacket(event)
+  routeThisPacket(event,True)
   checkIfNeedToDrop(event)
-  
 def _handle_ConnectionDown(event):
   print "Switch %s disconnected" % dpidToStr(event.dpid)
 
